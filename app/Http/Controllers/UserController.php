ebefdb5de8e2d3020545e83336efee584c3865e0
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-09-20 08:44:30 +0700

    Pertemuan 4 - Praktikum 2.1 – Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,15 +19,19 @@ class UserController extends Controller
         // $user = UserModel::all(); //ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
 
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
