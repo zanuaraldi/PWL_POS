3b41c4189b15675ed5690a8579b34d22bfadc42d
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-09-20 09:34:38 +0700

    Pertemuan 4 - Praktikum 2.5 – Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,39 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id' => 2
+                'level_id' => 2,
             ],
         );
+        $user->username = 'manager12';
+        
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // false
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); //true
+
+        // $user->isDirty(); //true
+        // $user->isDirty('username'); //true
+        // $user->isDirty('nama'); //false
+        // $user->isDirty(['nama', 'username']); //true
+
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
+
+        // $user->save();
+
+        // $user->isDirty(); //false
+        // $user->isClean(); // true
+        // dd($user->isDirty);
+
     }
 }
