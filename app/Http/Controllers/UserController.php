6ab9078032e61a6a872f3a237049c74f791cd636
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2024-09-28 11:45:37 +0700

    Pertemuan 5 - Praktikum 4 – Implementasi Filtering Datatables

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,11 +22,19 @@ class UserController extends Controller
         ];
 
         $activeMenu = 'user'; // set menu yang sedang aktif
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+
+        $level = LevelModel::all(); // ambil data level untuk filter level
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
 
     public function list(Request $request){
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
+
+        //Filter data user berdasarkan level_id
+        if($request->level_id){
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,13 +9,27 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="card-body">
-                @if(session('success'))
-                <div class="alert alert-success">{{ session('success') }}</div>
-                @endif
-                @if (session('error'))
-                <div class="alert alert-danger">{{ session('error') }}</div>
-                @endif
+            @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+            <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select name="level_id" id="level_id" class="form-control" required>
+                                <option value="">- Semua -</option>
+                                @foreach ($level as $item)
+                                    <option value="{{ $item->level_id }}">{{ $item->level_id }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
+                </div>
             </div>
             <table class="table table-border table-striped table-hover table-sm" id="table_user">
                 <thead>
@@ -44,7 +58,10 @@
                 ajax:{
                     "url": "{{ url('user/list') }}",
                     "dataType": "json",
-                    "type": "POST"
+                    "type": "POST",
+                    "data": function (d){
+                        d.level_id = $('#level_id').val();
+                    }
                 },
                 columns:[
                     {
@@ -78,6 +95,9 @@
                         searchable: false
                     }
                 ]
+            });
+            $('#level_id').on('change', function(){
+                dataUser.ajax.reload();
             });
         });
     </script>
